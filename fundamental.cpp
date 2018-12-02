96faf40aae3be61b18d023114c0ca516ba4fc96a
Basel <basel@umich.edu>
2018-12-01 21:47:21 -0500

    getting good fundamentals

--- fundamental.cpp
+++ fundamental.cpp
@@ -62,15 +62,6 @@ void normPoints(Mat& m1, Mat& m2, Mat& T1, Mat& T2)
   normColumn(m2, 0, mn_x2, avg_dist_x2);
   normColumn(m2, 1, mn_y2, avg_dist_y2);
 
-  std::cout << "mn_x1: " << mn_x1 << std::endl;
-  std::cout << "mn_y1: " << mn_y1 << std::endl;
-  std::cout << "mn_x2: " << mn_x2 << std::endl;
-  std::cout << "mn_y2: " << mn_y2 << std::endl;
-  std::cout << "avg_dist_x1: " << avg_dist_x1 << std::endl;
-  std::cout << "avg_dist_y1: " << avg_dist_y1 << std::endl;
-  std::cout << "avg_dist_x2: " << avg_dist_x2 << std::endl;
-  std::cout << "avg_dist_y2: " << avg_dist_y2 << std::endl;
-
   double data11[9] = {1/avg_dist_x1, 0, 0, 0, 1/avg_dist_y1, 0, 0, 0, 1.0f};
   Mat scale1( 3, 3, CV_64F, data11 );
   double data12[9] = {1, 0, -mn_x1, 0, 1, -mn_y1, 0, 0, 1};
@@ -85,7 +76,6 @@ void normPoints(Mat& m1, Mat& m2, Mat& T1, Mat& T2)
   T2 = scale2 * shift2;
 }
 
-
 std::vector<Mat> run7Point( Mat _m1, Mat _m2)
 {
   double a[7*9], w[7], u[9*9], v[9*9], c[4], r[3] = {0};
@@ -97,10 +87,9 @@ std::vector<Mat> run7Point( Mat _m1, Mat _m2)
   Mat W( 7, 1, CV_64F, w );
   Mat coeffs( 1, 4, CV_64F, c );
   Mat roots( 1, 3, CV_64F, r );
-  Mat T1(3, 3, CV_64F), T2(3, 3, CV_64F);
+  Mat T1 = Mat::eye(3, 3, CV_64F);
+  Mat T2 = Mat::eye(3, 3, CV_64F);
   std::vector<Mat> ret = std::vector<Mat>();
-  const Point2f* m1 = _m1.ptr<Point2f>();
-  const Point2f* m2 = _m2.ptr<Point2f>();
   int i, k, n;
 
   normPoints(_m1, _m2, T1, T2);
@@ -109,8 +98,8 @@ std::vector<Mat> run7Point( Mat _m1, Mat _m2)
   // the equation: (m2[i], 1)'*F*(m1[i], 1) = 0
   for( i = 0; i < 7; i++ )
   {
-      double x0 = m1[i].x, y0 = m1[i].y;
-      double x1 = m2[i].x, y1 = m2[i].y;
+      double x0 = _m1.at<double>(i, 0), y0 = _m1.at<double>(i, 1);
+      double x1 = _m2.at<double>(i, 0), y1 = _m2.at<double>(i, 1);
 
       a[i*9+0] = x1*x0;
       a[i*9+1] = x1*y0;
@@ -122,10 +111,6 @@ std::vector<Mat> run7Point( Mat _m1, Mat _m2)
       a[i*9+7] = y0;
       a[i*9+8] = 1;
   }
-
-  std::cout << _m1 << std::endl;
-  std::cout << _m2 << std::endl;
-  std::cout << A << std::endl;
 
   // A*(f11 f12 ... f33)' = 0 is singular (7 equations for 9 variables), so
   // the solution is linear subspace of dimensionality 2.
@@ -172,11 +157,8 @@ std::vector<Mat> run7Point( Mat _m1, Mat _m2)
 
   c[0] = f1[0]*t0 - f1[1]*t1 + f1[2]*t2;
 
-
   // solve the cubic equation; there can be 1 to 3 roots ...
   n = solveCubic( coeffs, roots );
-
-  std::cout << "n: " << n << std::endl;
 
   if( n < 1 || n > 3 )
       return ret;
@@ -203,7 +185,18 @@ std::vector<Mat> run7Point( Mat _m1, Mat _m2)
           data[i] = f1[i]*lambda + f2[i]*mu;
 
       Mat tmp(3,3,CV_64F, data);
-      ret.push_back((T2.t() * tmp) * T1);
+      Mat tmp2 = (T2.t() * tmp) * T1;
+      ret.push_back(tmp2 / tmp2.at<double>(2,2));
+
+      /* test of x'^T * F * x = 0
+      for (int i = 0; i < 7; i++)
+      {
+        double p1d[3] = {_m1.at<double>(i, 0), _m1.at<double>(i, 1), 1};
+        double p2d[3] = {_m2.at<double>(i, 0), _m2.at<double>(i, 1), 1};
+        Mat p1(3,1,CV_64F,p1d), p2(3,1,CV_64F,p2d);
+        std::cout << "test: " << p2.t() * tmp * p1 << std::endl;
+      }
+      */
   }
 
   return ret;
@@ -231,7 +224,6 @@ int main( int argc, char** argv )
   detector->detectAndCompute( img_1, Mat(), keypoints_1, descriptors_1 );
   detector->detectAndCompute( img_2, Mat(), keypoints_2, descriptors_2 );
 
-
   //-- Step 2: Matching descriptor vectors using FLANN matcher
   FlannBasedMatcher matcher;
   std::vector< std::vector< DMatch > > matches;
@@ -253,15 +245,22 @@ int main( int argc, char** argv )
   // RANSAC to find best fundamental matrix
   srand(time(NULL));
   unsigned int n_matches = good_matches.size();
-  unsigned int its = 1;
-  for (unsigned int it = 0 ; it < its ; it++)
+  unsigned int its = 3000;
+  unsigned int n_inliers = 0;
+  unsigned int best_inliers = 0;
+  Mat best_F(3,3,CV_64F);
+  double threshold = 0.0000001;
+  for (unsigned int it = 0; it < its; it++)
   {
     Mat pts1(7,2,CV_64F);
     Mat pts2(7,2,CV_64F);
+    std::vector<unsigned int> inds = std::vector<unsigned int>();
     // select matches and set up matrices
     for (unsigned int i = 0; i < 7; i++)
     {
-      DMatch* this_match = &good_matches[rand() % n_matches];
+      unsigned int ind = rand() % n_matches;
+      inds.push_back(ind);
+      DMatch* this_match = &good_matches[ind];
       Point2f pt1 = keypoints_1[this_match->queryIdx].pt;
       Point2f pt2 = keypoints_2[this_match->trainIdx].pt;
       pts1.at<double>(i,0) = (double)pt1.x;
@@ -269,19 +268,131 @@ int main( int argc, char** argv )
       pts2.at<double>(i,0) = (double)pt2.x;
       pts2.at<double>(i,1) = (double)pt2.y;
     }
+
+    // compute possible Fs based on those matches
+    std::vector<Mat> sols = run7Point(pts1.clone(), pts2.clone());
     
-    std::cout << pts1 << std::endl;
+    // for each solution, try it out
+    for (int i = 0; i < sols.size(); i++)
+    {
+      // init
+      n_inliers = 0;
+      Mat F = sols[i];
+      // std::cout << F << std::endl;
 
-    // compute F based on those matches
-    std::vector<Mat> sols = run7Point(pts1, pts2);
-    for (int i = 0 ; i < sols.size(); i++)
-      std::cout << sols[i] << std::endl;
-    std::cout << "2" << std::endl;
-    // compute number of inliers
+      // check each point to see if it's an inlier
+      for (int j = 0; j < n_matches; j++)
+      // for (int j = 0; j < n_matches; j++)
+      {
+        // get point info
+        // unsigned int ind = inds[j];
+        unsigned int ind = j;
+        DMatch* this_match = &good_matches[ind];
+        Point2f pt1 = keypoints_1[this_match->queryIdx].pt;
+        Point2f pt2 = keypoints_2[this_match->trainIdx].pt;
 
-  }
+        double pt1_d[3] = {pt1.x, pt1.y, 1};
+        Mat pt1_m(3,1,CV_64F, pt1_d);
+        double pt2_d[3] = {pt2.x, pt2.y, 1};
+        Mat pt2_m(3,1,CV_64F, pt2_d);
+
+        /* test of x'^T * F * x = 0
+        */
+        // std::cout << "test: " << pt2_m.t() * F * pt1_m << std::endl;
+
+        // get epipolar lines and normalize
+        Mat l1(3,1,CV_64F);
+        Mat l2(3,1,CV_64F);
+        l1 = pt2_m.t() * F;
+        l1 /= (l1.at<double>(0,0)*l1.at<double>(0,0) +
+               l1.at<double>(0,1)*l1.at<double>(0,1) +
+               l1.at<double>(0,2)*l1.at<double>(0,2));
+        l2 = F * pt1_m;
+        l2 /= (l2.at<double>(0,0)*l2.at<double>(0,0) +
+               l2.at<double>(1,0)*l2.at<double>(1,0) +
+               l2.at<double>(2,0)*l2.at<double>(2,0));
+
+        Mat e1 = l1 * pt1_m;
+        Mat e2 = pt2_m.t() * l2;
+        double dist = (abs(e1.at<double>(0,0)) + 
+                       abs(e2.at<double>(0,0))) / 2.0f;
+        // std::cout << "pt1: " << pt1_m << std::endl;
+        // std::cout << "test2: " << dist << std::endl;
+
+        if (dist < threshold) n_inliers++;
+      } // end for each point
+
+      if (n_inliers > best_inliers)
+      {
+        best_inliers = n_inliers;
+        best_F = F.clone();
+      }
+    } // end for each solution
+
+  } // end for each iteration
+
+  // show results
+  std::cout << "best F: " << best_F << std::endl;
+  std::cout << best_inliers << " out of " << n_matches << std::endl;
+
+  std::vector<KeyPoint> inliers_1 = std::vector<KeyPoint>();
+  std::vector<KeyPoint> inliers_2 = std::vector<KeyPoint>();
+  std::vector<DMatch> inlier_matches = std::vector<DMatch>();
+  // check each point to see if it's an inlier
+  for (int j = 0; j < n_matches; j++)
+  // for (int j = 0; j < n_matches; j++)
+  {
+    // get point info
+    // unsigned int ind = inds[j];
+    unsigned int ind = j;
+    DMatch* this_match = &good_matches[ind];
+    Point2f pt1 = keypoints_1[this_match->queryIdx].pt;
+    Point2f pt2 = keypoints_2[this_match->trainIdx].pt;
+
+    double pt1_d[3] = {pt1.x, pt1.y, 1};
+    Mat pt1_m(3,1,CV_64F, pt1_d);
+    double pt2_d[3] = {pt2.x, pt2.y, 1};
+    Mat pt2_m(3,1,CV_64F, pt2_d);
+
+    /* test of x'^T * F * x = 0
+    */
+    // std::cout << "test: " << pt2_m.t() * F * pt1_m << std::endl;
+
+    // get epipolar lines and normalize
+    Mat l1(3,1,CV_64F);
+    Mat l2(3,1,CV_64F);
+    l1 = pt2_m.t() * best_F;
+    l1 /= (l1.at<double>(0,0)*l1.at<double>(0,0) +
+            l1.at<double>(0,1)*l1.at<double>(0,1) +
+            l1.at<double>(0,2)*l1.at<double>(0,2));
+    l2 = best_F * pt1_m;
+    l2 /= (l2.at<double>(0,0)*l2.at<double>(0,0) +
+            l2.at<double>(1,0)*l2.at<double>(1,0) +
+            l2.at<double>(2,0)*l2.at<double>(2,0));
+
+    Mat e1 = l1 * pt1_m;
+    Mat e2 = pt2_m.t() * l2;
+    double dist = (abs(e1.at<double>(0,0)) + 
+                    abs(e2.at<double>(0,0))) / 2.0f;
+    // std::cout << "pt1: " << pt1_m << std::endl;
+    // std::cout << "test2: " << dist << std::endl;
+
+    if (dist < threshold)
+    {
+      DMatch to_add = DMatch(inliers_1.size(), inliers_1.size(), dist);
+      inliers_1.push_back(keypoints_1[this_match->queryIdx]);
+      inliers_2.push_back(keypoints_2[this_match->trainIdx]);
+      inlier_matches.push_back(to_add);
+    }
+  } // end for each point
+
+  Mat img_matches;
+  drawMatches( img_1, inliers_1, img_2, inliers_2,
+               inlier_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
+               std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
+  imshow( "Good Matches", img_matches );
+  waitKey(0);
   
-  std::cout << "1" << std::endl;
 }
 /*
  * @function readme
